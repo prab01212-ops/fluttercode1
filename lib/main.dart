32d011b860a5bae64f43f668c3a42dbd0826167b
prab01212-ops <prab01212@example.com>
2025-08-20 20:07:19 +0530

    this is Flutter practice

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -31,38 +31,172 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Center(child: Text('flutter Project ')),
+          title: Center(child: Text('flutter Project ', style: TextStyle(fontWeight: FontWeight.bold),)),
         ),
-        body: Container(
+        body:Container(
           width: double.infinity,
           height: double.infinity,
-          //color: Colors.blue.shade50,
-          child: Center(
-            child: Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                //borderRadius: BorderRadius.circular(9),
-                border: Border.all(
-                    width: 3,
-                    color: Colors.black
+          color: Colors.redAccent.shade100,
+          child:SingleChildScrollView(
+            child: Column(
+             // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top:40),
+                  width: 300,
+                  height: 400,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset("assets/images/myphoto.jpg"),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text("Name"),Text("Age")
+                            ],
+                          ),
+                        ),
+
+                      ],
+                    ),
+
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade100,
+                    borderRadius: BorderRadius.circular(11),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                    boxShadow:[
+                      BoxShadow(
+                        color: Colors.black,
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      )
+                    ],
+                    shape: BoxShape.rectangle,
+                  ),
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 11,
-                    spreadRadius: 11,
-                    color: Colors.blue,
-                  )
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  width: 300,
+                  height: 300,
+                  child: Image.asset("assets/images/flutter.png"),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: BorderRadius.circular(11),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                    boxShadow:[
+                      BoxShadow(
+                        color: Colors.black,
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      )
+                    ],
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  width: 300,
+                  height: 300,
+                  child: Image.asset("assets/images/flutter1.png"),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: BorderRadius.circular(11),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                    boxShadow:[
+                      BoxShadow(
+                        color: Colors.black,
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      )
+                    ],
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top:40),
+                  width: 300,
+                  height: 300,
+                  child: Image.asset("assets/images/myphoto.jpg"),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: BorderRadius.circular(11),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                    boxShadow:[
+                      BoxShadow(
+                        color: Colors.black,
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      )
+                    ],
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  width: 300,
+                  height: 300,
+                  child: Image.asset("assets/images/flutter.png"),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: BorderRadius.circular(11),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                    boxShadow:[
+                      BoxShadow(
+                        color: Colors.black,
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      )
+                    ],
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  width: 300,
+                  height: 300,
+                  child: Image.asset("assets/images/flutter1.png"),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade50,
+                    borderRadius: BorderRadius.circular(11),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                    boxShadow:[
+                      BoxShadow(
+                        color: Colors.black,
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      )
+                    ],
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
                 ],
-                shape: BoxShape.circle,
-              ),
             ),
           ),
-        )
+          ),
     );
   }
 }
